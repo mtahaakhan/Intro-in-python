f64dc69ba5ea77577b24be6cbb9a1c45eadc9d96
tahaadotdev <muhammadtahakhan98@gmail.com>
2019-10-19 14:42:10 +0500

    Added Function in Turtle.py

--- turtle.py
+++ turtle.py
@@ -1,11 +1,32 @@
 import turtle
 
 
+# Here we were making a turtle 
+# my_turtle = turtle.Turtle()
+# my_turtle.forward(100)
+# my_turtle.left(90)      
+# my_turtle.forward(100)
+# my_turtle.left(90)
+# my_turtle.forward(100)
+# my_turtle.left(90)
+# my_turtle.forward(100)
+
+
+
+
+# Now we are using function in it
+
+
+
 my_turtle = turtle.Turtle()
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
-my_turtle.left(90)
-my_turtle.forward(100)
+
+def square():
+    my_turtle.forward(100)
+    my_turtle.left(90)      
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+    my_turtle.left(90)
+    my_turtle.forward(100)
+
+square()
