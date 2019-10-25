87b9904dc3b7e6ab9e463be5122c741b3c6a3450
tahaadotdev <muhammadtahakhan98@gmail.com>
2019-10-25 16:43:39 +0500

    Type Conversion with int() & str()

---
+++ type-conversion.py
@@ -0,0 +1,31 @@
+# EXAMPLE NO: 1
+# pi = 3.14159
+# print(pi)
+
+
+# EXAMPLE NO: 2
+# Both variables are int
+# first_num = 5
+# second_num = 3
+# print(first_num + second_num)
+
+
+# EXAMPLE NO: 3
+# If we are taking input from user, the variable is a string
+# num1 = input('Enter first number')
+# num2 = input('Enter second number')
+# print(num1 + num2)
+
+
+# EXAMPLE NO: 4
+# But now both strings are converted into integars. This is called type conversion
+# num1 = input('Enter first number')
+# num2 = input('Enter second number')
+# print(int(num1) + int(num2))
+
+
+
+# EXAMPLE NO: 5
+# Here the variable is integar and Python be like : What the heck? How can i concatenate int with string? that's why i have converted int with str() to convert the integar into string
+# days_in_feb = 28
+# print('There are ' + str(days_in_feb) + ' days in February')
